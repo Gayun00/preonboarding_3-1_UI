e66ef5b5abf0cd98c18152766def4bf5e4e3bf79
gy <gayunlee11@gmail.com>
2022-02-14 19:42:24 +0900

    Feat: 뒤로가기 버튼 UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import BackButton from "./components/BackButton";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BackButton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styled";
+
+function BackButton() {
+  return <S.Button></S.Button>;
+}
+
+export default BackButton;
